24f4559c3ffca7a816f9de34428a1e954592c41d
kenny <kennyquintana297@gmail.com>
2021-06-25 03:12:49 -0400

    complete la vista de detalleClientesRegulares

--- src/Components/ButtonRadio.tsx
+++ src/Components/ButtonRadio.tsx
@@ -1,21 +1,29 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { RadioButton, Text } from 'react-native-paper';
 
 const ButtonRadio = () => {
   const [value, setValue] = React.useState('first');
 
   return (
-    <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value}>
-      <View>
-        <Text>First</Text>
-        <RadioButton value="first" />
-      </View>
-      <View>
-        <Text>Second</Text>
-        <RadioButton value="second" />
+    <RadioButton.Group onValueChange={newValue => setValue(newValue)} value={value} >
+       <View style={styles.container}>
+        <View>
+          <Text>Mayorista</Text>
+          <RadioButton value="first" />
+        </View>
+        <View>
+          <Text>Empresario</Text>
+          <RadioButton value="second" />
+        </View>
       </View>
     </RadioButton.Group>
   );
 };
+
+const styles=StyleSheet.create({
+  container:{
+    flexDirection:"row"
+  }
+});
 export default ButtonRadio;
---
+++ src/Components/FabGroup.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { FAB, Portal, Provider } from 'react-native-paper';
+
+const Fabgr = () => {
+  const [state, setState] = React.useState({ open: false });
+
+  const onStateChange = ({ open }:any) => setState({ open });
+
+  const { open } = state;
+
+  return (
+    <Provider>
+      <Portal>
+        <FAB.Group
+        visible={true}
+
+          open={open}
+          icon={open ? 'calendar-today' : 'folder'}
+          actions={[
+            
+            
+            {
+              icon: 'account-edit',
+              label: 'Eliminar',
+              onPress: () => console.log('Pressed eliminar'),
+            },
+            {
+                icon: 'account-edit',
+                label: 'Editar',
+                onPress: () => console.log('Pressed editar'),
+                small: false
+              },
+          ]}
+          onStateChange={onStateChange}
+          onPress={() => {
+            if (open) {
+              // do something if the speed dial is open
+            }
+          }}
+        />
+      </Portal>
+    </Provider>
+  );
+};
+
+export default Fabgr;
